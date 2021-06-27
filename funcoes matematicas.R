b739a22df9352782eb53e93b00ba4f21a38f39ff
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-27 16:43:25 -0300

    Terceira aula

---
+++ funcoes matematicas.R
@@ -0,0 +1,24 @@
+a <- 10
+
+sqrt(a) #Raiz quadrada
+
+factorial(a) #fatorial
+
+b <- -6
+abs(b) #Retorna o número absoluto
+
+log(a) #Retorna o logaritmo na base 10
+
+log(a, base=4) #Retorna o logaritmo na base configurada
+
+c <- 5.6
+round(c) #Arredonda o número para o inteiro mais próximo
+
+d <-5.42335
+round(d, digits=2) #Arredonda o número para o número mais próximo de acordo com tamanho configurado
+signif(d, digits=2) #Arredonda o número para o número mais próximo de acordo com tamanho configurado
+
+e <- 4.5
+floor(e) #arredonda para o inteiro mais baixo
+ceiling(e) #arredonda para o inteiro mais alto
+trunc(e) #corta o número quebrado e mantém apenas o inteiro

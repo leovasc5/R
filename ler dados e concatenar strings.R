b7f2b372862677c8f59f505dec3d71dc01c24b70
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-27 17:36:38 -0300

    Add files via upload

---
+++ ler dados e concatenar strings.R
@@ -0,0 +1,10 @@
+b <- "Leonardo"
+c <- "Vasconcelos"
+d <- paste(b, c)
+d
+
+nome <- readline("Digite seu nome: ")
+idade <- readline("Informe a sua idade: ")
+idade <- as.integer(idade) #Converteu para inteiro
+res <- paste(nome, " tem ", idade, " anos!")
+print(res)

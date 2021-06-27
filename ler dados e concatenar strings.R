d8a59c9972c7c3083f877dbd0840a9da4a9550ac
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-27 17:37:09 -0300

    Quinta aula

--- ler dados e concatenar strings.R
+++ ler dados e concatenar strings.R
@@ -7,4 +7,5 @@ nome <- readline("Digite seu nome: ")
 idade <- readline("Informe a sua idade: ")
 idade <- as.integer(idade) #Converteu para inteiro
 res <- paste(nome, " tem ", idade, " anos!")
+
 print(res)

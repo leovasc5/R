44209143192df5e07d4f519b761c000878b86998
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-27 16:11:52 -0300

    Primeira aula

---
+++ operadores aritméticos básicos e lei de pemdas.R
@@ -0,0 +1,18 @@
+5 + 4 #Adição
+5 - 9 #Subtração
+5 * 12 #Multiplicação
+9 / 5 #Divisão
+3^3 #Expenciação
+9%%4 #Resto da divisão
+9%/%4 #Resto inteiro
+
+#Regra de PEMDAS -> ordem das equações
+3+4-2*4/6
+3+(4-2)*4/6
+
+#Teste de Baskhara
+a <- 1
+b <- 4
+c <- 2
+
+b^2-4*a*c 

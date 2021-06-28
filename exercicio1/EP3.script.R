00dc30171748bdfb33520e9d625a7ee3bc23cf95
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-28 13:28:35 -0300

    Add files via upload

--- exercicio1/EP3.script.R
+++ exercicio1/EP3.script.R
@@ -49,7 +49,9 @@ setor_maior_percentual_contratacoes_regiao <- function(df, regiao){
       }
     }
   }
-  return(paste(setor, " -- ", maior))
+  
+  return(paste("Na Região ", regiao, ", o setor que mais contrata é o ", setor, " com uma taxa de ", maior))
+
 }
 
 print(setor_maior_percentual_contratacoes_regiao(df_ocupadosReg, "Norte"))
@@ -61,5 +63,107 @@ print(setor_maior_percentual_contratacoes_regiao(df_ocupadosReg, "Nordeste"))
 print(setor_maior_percentual_contratacoes_regiao(df_ocupadosReg, "Sudeste"))
 
 print(setor_maior_percentual_contratacoes_regiao(df_ocupadosReg, "CentroOeste"))
+
+
+#### EXERCÍCIO 4 ####
+compara_regional_nacional <- function(df_ocupadosNac, df_ocupadosReg){
+  maiorNorte <- 0
+  setorNorte <- ""
+  nomeSetorNorte <- ""
+  
+  maiorSul <- 0
+  setorSul <- ""
+  nomeSetorSul <- ""
+  
+  maiorNordeste <- 0
+  setorNordeste <- ""
+  nomeSetorNordeste <- ""
+  
+  maiorSudeste <- 0
+  setorSudeste <- ""
+  nomeSetorSudeste <- ""
+  
+  maiorCO <- 0
+  setorCO <- ""
+  nomeSetorCO <- ""
+  
+  percentual_nac_norte <- 0
+  percentual_nac_sul <- 0
+  percentual_nac_nordeste <- 0
+  percentual_nac_sudeste <- 0
+  percentual_nac_co <- 0
+  
+  #Looping Geral
+  for(a in 1:nrow(df_ocupadosReg)){
+    if(df_ocupadosReg[a,1] == "Norte"){
+      if(df_ocupadosReg[a, 4] > maiorNorte){
+        maiorNorte <- df_ocupadosReg[a, 4]
+        setorNorte <- df_ocupadosReg[a, 2]
+        nomeSetorNorte <- df_ocupadosReg[a, 2]
+        for(b in 1:nrow(df_ocupadosNac)){
+          if(df_ocupadosNac[b, 2] == setorNorte){
+            percentual_nac_norte <- df_ocupadosNac[b, 4]
+          }
+        }
+      }
+    }else if(df_ocupadosReg[a,1] == "Sul"){
+      if(df_ocupadosReg[a, 4] > maiorSul){
+        maiorSul <- df_ocupadosReg[a, 4]
+        setorSul <- df_ocupadosReg[a, 2]
+        nomeSetorSul <- df_ocupadosReg[a, 2]
+        for(b in 1:nrow(df_ocupadosNac)){
+          if(df_ocupadosNac[b, 2] == setorSul){
+            percentual_nac_sul <- df_ocupadosNac[b, 4]
+          }
+        }
+      }
+    }else if(df_ocupadosReg[a,1] == "Nordeste"){
+      if(df_ocupadosReg[a, 4] > maiorNordeste){
+        maiorNordeste <- df_ocupadosReg[a, 4]
+        setorNordeste <- df_ocupadosReg[a, 2]
+        nomeSetorNordeste <- df_ocupadosReg[a, 2]
+        for(b in 1:nrow(df_ocupadosNac)){
+          if(df_ocupadosNac[b, 2] == setorNordeste){
+            percentual_nac_nordeste <- df_ocupadosNac[b, 4]
+          }
+        }
+      }
+    }else if(df_ocupadosReg[a,1] == "Sudeste"){
+      if(df_ocupadosReg[a, 4] > maiorSudeste){
+        maiorSudeste <- df_ocupadosReg[a, 4]
+        setorSudeste <- df_ocupadosReg[a, 2]
+        nomeSetorSudeste <- df_ocupadosReg[a, 2]
+        for(b in 1:nrow(df_ocupadosNac)){
+          if(df_ocupadosNac[b, 2] == setorNordeste){
+            percentual_nac_nordeste <- df_ocupadosNac[b, 4]
+          }
+        }
+      }
+    }else if(df_ocupadosReg[a,1] == "CentroOeste"){
+      if(df_ocupadosReg[a, 4] > maiorCO){
+        maiorCO <- df_ocupadosReg[a, 4]
+        setorCO <- df_ocupadosReg[a, 2]
+        nomeSetorCO <- df_ocupadosReg[a, 2]
+        for(b in 1:nrow(df_ocupadosNac)){
+          if(df_ocupadosNac[b, 2] == setorCO){
+            percentual_nac_co <- df_ocupadosNac[b, 4]
+          }
+        }
+      }
+    }
+  }
+
+  
+  print("      Região   Setor  %Regional  %Nacional")
+  print(paste("       Norte  ", nomeSetorNorte, "         ", maiorNorte, "", maiorNorte))
+  print(paste("         Sul  ", nomeSetorSul, "          ", maiorSul, "", maiorSul))
+  print(paste("    Nordeste  ", nomeSetorNordeste, "          ", maiorNordeste, "", maiorNordeste))
+  print(paste("     Sudeste  ", nomeSetorSudeste, "          ", maiorSudeste, "", maiorSudeste))
+  print(paste("Centro-Oeste  ", nomeSetorCO, "          ", maiorCO, "", maiorCO))
+}
+
+compara_regional_nacional(df_ocupadosNac, df_ocupadosReg)
+
+
 
 
